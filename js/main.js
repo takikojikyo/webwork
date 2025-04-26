51c1714f3023e809ed8782ec0c5f5234d64b31b6
takikojikyo <149746810+takikojikyo@users.noreply.github.com>
2025-04-27 07:01:33 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -6,3 +6,29 @@ window.addEventListener('scroll',()=>{
     fixedMenu.classList.remove('active');
   }
 });
+
+
+const animateFade=(entries,obs)=>{
+  entries.forEach((entry)=>{
+    if(entry.isIntersecting){
+    // console.log(entry.target);
+    entry.target.animate(
+      {
+        opacity:[0,1],
+        translate:['0 4rem',0],
+      },
+      {
+        duration:1000,
+        easing:'ease',
+        fill:'forwards',
+      }
+    );
+    obs.unobserve(entry.target);
+  }
+  });
+};
+const fadeObserver=new IntersectionObserver(animateFade);
+const fadeElements=document.querySelectorAll('.fadein')
+fadeElements.forEach((fadeElement)=>{
+  fadeObserver.observe(fadeElement);
+})
